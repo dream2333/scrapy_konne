899e11bd268ff5ce1a5ddd1276efa29df5079639
dream2333 <vincentqng@gmail.com>
2024-06-26 10:24:49 +0800

    支持v2本地代理

--- scrapy_konne/middlewares/__init__.py
+++ scrapy_konne/middlewares/__init__.py
@@ -1,4 +1,5 @@
 from .dupefilter import UrlRedisDupefilterMiddleware
 from .proxypool import ProxyPoolDownloaderMiddleware
+from .v2ray import V2rayProxyMiddleware
 
-__all__ = ["UrlRedisDupefilterMiddleware", "ProxyPoolDownloaderMiddleware"]
+__all__ = ["UrlRedisDupefilterMiddleware", "ProxyPoolDownloaderMiddleware", "V2rayProxyMiddleware"]
