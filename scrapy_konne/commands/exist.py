bef01197444fa941175c04cbce9a0daf1f3fc3e0
dream2333 <vincentqng@gmail.com>
2024-06-27 17:13:51 +0800

    更新检查入库命令插件

--- scrapy_konne/commands/exist.py
+++ scrapy_konne/commands/exist.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from redis import Redis
 import requests
 from scrapy.commands import ScrapyCommand
@@ -8,14 +9,20 @@ from scrapy_konne.utils.fingerprint import get_url_fp
 settings = get_project_settings()
 
 
-def redis_filter(url, source):
+def redis_url_filter(url, source):
     redis_url = settings.get("REDIS_URL")
     client = Redis.from_url(redis_url)
     hash_value = get_url_fp(url)
     result = client.zscore(f"dupefilter:{source}", hash_value)
-    if result:
-        return True
-    return False
+    return result//1000
+
+
+
+def redis_cursor_filter(cursor, source):
+    redis_url = settings.get("REDIS_URL")
+    client = Redis.from_url(redis_url)
+    result = client.zscore(f"dupefilter:{source}", cursor)
+    return result//1000
 
 
 def url_is_exist(url):
@@ -44,6 +51,7 @@ class Exist(ScrapyCommand):
         return "查看某个链接或id是否已进入去重库"
 
     def run(self, args, opts):
+        is_url = True
         try:
             if len(args) != 0:
                 raise UsageError("参数数量不对")
@@ -55,19 +63,25 @@ class Exist(ScrapyCommand):
             try:
                 int(dup_value)
                 print("输入的是自增id，仅进行redis去重查询，如需继续确认是否在康奈库中存在，请输入url")
+                is_url = False
             except ValueError:
                 print("输入的是url，进行url查询")
-                dup_value = dup_value
                 result = url_is_exist(dup_value)
                 if result:
                     print("\033[95m在康奈去重库已存在\033[0m")
                 else:
                     print("\033[96m在康奈去重库不存在\033[0m")
             spider_name = input("请输入爬虫类变量name，以继续进行redis查询: ")
-            result = redis_filter(dup_value, spider_name)
-            if result:
-                print(f"\033[91m在redis去重库[{spider_name}]已存在\033[0m")
+            if is_url:
+                result = redis_url_filter(dup_value, spider_name)
             else:
-                print(f"\033[92m在redis去重库[{spider_name}]不存在\033[0m")
+                result = redis_cursor_filter(dup_value, spider_name)
+            if result:
+                dt_object = datetime.fromtimestamp(result)
+                # 将datetime对象格式化为字符串
+                date_string = dt_object.strftime('%Y-%m-%d %H:%M:%S')
+                print(f"\033[92m在redis去重库[{spider_name}]已存在: {date_string}\033[0m")
+            else:
+                print(f"\033[91m在redis去重库[{spider_name}]不存在\033[0m:")
         except KeyboardInterrupt:
             print("\n用户主动中断")
