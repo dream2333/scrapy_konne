3fec3df6707d4e5628148a55f1f16143b891e18d
dream2333 <vincentqng@gmail.com>
2024-07-01 09:39:11 +0800

    command支持linux

--- scrapy_konne/commands/deploy.py
+++ scrapy_konne/commands/deploy.py
@@ -5,7 +5,7 @@ from scrapy.commands import ScrapyCommand
 
 
 def deploy_project():
-    command = ["scrapyd-deploy.exe"]
+    command = ["scrapyd-deploy"]
     process = subprocess.Popen(command, stdout=subprocess.PIPE)
     output, error = process.communicate()
 
