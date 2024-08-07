2e5cd634cc5a51a6b8f1669f5cdf7585227991b6
dream2333 <vincentqng@gmail.com>
2024-08-07 16:58:19 +0800

    sitemapspider进一步更新，更新版本号

--- scrapy_konne/spiders/sitemap.py
+++ scrapy_konne/spiders/sitemap.py
@@ -37,6 +37,13 @@ class SitemapSpider(ScrapySitemapSpider):
     expired_days: int = 5
     """sitemap中的url如果距离现在超过expired_days天，则不再抓取"""
 
+    def expired_check(self, time_str: str) -> bool:
+        if time_str:
+            modtime = parse(time_str).astimezone()
+            if datetime.now().astimezone() - modtime > timedelta(days=self.expired_days):
+                return True
+        return False
+
     def _parse_sitemap(self, response):
         if response.url.endswith("/robots.txt"):
             for url in sitemap_urls_from_robots(response.text, base_url=response.url):
@@ -56,10 +63,8 @@ class SitemapSpider(ScrapySitemapSpider):
                 for entry in it:
                     loc = entry["loc"]
                     modtime_str = entry.get("lastmod")
-                    if modtime_str:
-                        modtime = parse(entry["lastmod"])
-                        if datetime.now().astimezone() - modtime > timedelta(days=self.expired_days):
-                            continue
+                    if self.expired_check(modtime_str):
+                        continue
                     if any(x.search(loc) for x in self._follow):
                         yield KRequest(loc, callback=self._parse_sitemap)
             elif s.type == "urlset":
@@ -67,10 +72,8 @@ class SitemapSpider(ScrapySitemapSpider):
                     modtime_str = entry.get("lastmod")
                     publication_date_str = entry.get("publication_date")
                     time_str = publication_date_str or modtime_str
-                    if time_str:
-                        modtime = parse(time_str)
-                        if datetime.now().astimezone() - modtime > timedelta(days=self.expired_days):
-                            continue
+                    if self.expired_check(modtime_str):
+                        continue
                     title = entry.get("title")
                     for r, c in self._cbs:
                         if r.search(entry["loc"]):
