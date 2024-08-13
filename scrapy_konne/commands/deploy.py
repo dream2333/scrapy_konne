bf0cf09b2c28054f0ed2bca064c854d6ac2ea5a5
dream2333 <vincentqng@gmail.com>
2024-08-13 14:59:33 +0800

    scrapyd集群部署

--- scrapy_konne/commands/deploy.py
+++ scrapy_konne/commands/deploy.py
@@ -5,33 +5,34 @@ from scrapy.commands import ScrapyCommand
 
 
 def deploy_project():
-    command = ["scrapyd-deploy","-a"]
+    command = ["scrapyd-deploy", "-a"]
     process = subprocess.Popen(command, stdout=subprocess.PIPE)
     output, error = process.communicate()
 
     if error:
         print(f"执行命令失败: {error}")
     else:
-        data = json.loads(output)
-        print("节点名称：", data["node_name"])
-        if data["status"] == "ok":
-            print(
-                "部署成功：",
-                data["project"],
-                "版本：",
-                data["version"],
-                "爬虫个数：",
-                data["spiders"],
-            )
-        else:
-            print(data["message"])
-            print("部署失败")
+        for line in output.strip().split(b"\n\n"):
+            data = json.loads(line)
+            print(f'===========节点名称：{data["node_name"]}===========')
+            if data["status"] == "ok":
+                print(
+                    "部署成功：",
+                    data["project"],
+                    "版本：",
+                    data["version"],
+                    "爬虫个数：",
+                    data["spiders"],
+                )
+            else:
+                print(data["message"])
+                print("部署失败")
 
 
 class Deploy(ScrapyCommand):
     requires_project = True
     default_settings = {"LOG_ENABLED": False}
-    
+
     def syntax(self):
         return "[option] 自定义项目名"
 
